d6b50207dd0e328af5a5dbcb1345d79c74614fb4
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2022-11-29 17:55:10 +0100

    add text highlights

--- components/intro.js
+++ components/intro.js
@@ -11,44 +11,45 @@ export default function Intro() {
       className={`px-10 py-10 flex  text-base md:text-3xl tracking-tighter  ${fontDigitalDream.className}`}
     >
       <p>
-        Welcome to <span className="text-gray-300 bg-black">GIRLY</span> / We're
-        a small team that builds software and explores a future of work that's
-        radically softer and sweeter / Connect with us to learn more about our
+        Welcome to <span className="text-black bg-yellow-100">GIRLY</span> /
+        We're a small team that builds software and explores a future of work
+        that's radically softer and sweeter / Connect with us to learn about our
         first project and how to get involved /{" "}
         <a
           href="https://twitter.com/GirlyDAO"
-          className="text-gray-500 hover:text-blue-700 duration-200 transition-colors"
+          className="text-gray-500 bg-teal-100 hover:text-violet-700 duration-200 transition-colors"
         >
           Twitter
         </a>{" "}
         /{" "}
         <a
           href="https://app.commonground.cg/community/NCTwbWjKE5"
-          className="text-gray-500 hover:text-blue-700 duration-200 transition-colors"
+          className="text-gray-500 bg-teal-100 hover:text-violet-700 duration-200 transition-colors"
         >
           Common Ground
         </a>{" "}
         (Team + community chat. Referral link{" "}
         <a
           href="https://alpha.commonground.cg/?ref=hpYWNFp7"
-          className="text-gray-500 hover:text-blue-700 duration-200 transition-colors"
+          className="text-gray-500 hover:text-violet-700 duration-200 transition-colors"
         >
           HERE
         </a>{" "}
         if you need one) /{" "}
         <a
           href="https://app.wonderverse.xyz/invite/8eXyL8MQALoTYw"
-          className="text-gray-500 hover:text-blue-700 duration-200 transition-colors"
+          className="text-gray-500 hover:text-violet-700 duration-200 transition-colors"
         >
           Wonder
         </a>{" "}
         (Public team tasks) /{" "}
         <a
           href="https://github.com/GIRLYDAO"
-          className="text-gray-500 hover:text-blue-700 duration-200 transition-colors"
+          className="text-gray-500 hover:text-violet-700 duration-200 transition-colors"
         >
           GitHub
-        </a>
+        </a>{" "}
+        / IRL :p
       </p>
     </section>
   );
