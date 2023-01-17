03ec078dc6f789755d6265512482a7cffdc2b70c
Sania García <santosuke@gmail.com>
2023-01-17 13:57:15 +0100

    update text, cg link and add layers

--- components/intro.js
+++ components/intro.js
@@ -11,35 +11,39 @@ export default function Intro() {
       className={`px-10 py-10 flex  text-base md:text-3xl tracking-tighter  ${fontDigitalDream.className}`}
     >
       <p>
-        Welcome to <span className="text-black bg-yellow-100">GIRLY</span> /
-        We're a small tech label building software and exploring a future of
-        work that's radically softer and sweeter / Connect with us to learn
-        about our first project and how to get involved / More info coming soon
-        /{" "}
+        <span className="text-black bg-white">GIRLY</span> is a small tech label
+        exploring a radically softer and sweeter future / First project:{" "}
+        <a
+          href="https://layers.live"
+          className="text-black bg-pink-100 hover:text-pink-500 duration-200 transition-colors"
+        >
+          Layers
+        </a>{" "}
+        / Connect on:{" "}
         <a
           href="https://twitter.com/GirlyDAO"
-          className="text-gray-500 bg-teal-100 hover:text-violet-700 duration-200 transition-colors"
+          className="text-gray-500 bg-teal-100 hover:text-teal-500 duration-200 transition-colors"
         >
           Twitter
         </a>{" "}
         /{" "}
         <a
-          href="https://app.commonground.cg/community/NCTwbWjKE5?ref=hpYWNFp7"
-          className="text-gray-500 bg-teal-100 hover:text-violet-700 duration-200 transition-colors"
+          href="https://app.cg/community/NCTwbWjKE5"
+          className="text-gray-500 bg-teal-100 hover:text-teal-500 duration-200 transition-colors"
         >
           Common Ground
         </a>{" "}
         /{" "}
         <a
           href="https://app.wonderverse.xyz/invite/8eXyL8MQALoTYw"
-          className="text-gray-500 hover:text-violet-700 duration-200 transition-colors"
+          className="text-gray-500 hover:text-teal-500 duration-200 transition-colors"
         >
           Wonder
         </a>{" "}
         /{" "}
         <a
           href="https://github.com/GIRLYDAO"
-          className="text-gray-500 hover:text-violet-700 duration-200 transition-colors"
+          className="text-gray-500 hover:text-teal-500 duration-200 transition-colors"
         >
           GitHub
         </a>{" "}
