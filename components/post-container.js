1126c6b2528f2c408032dafd16dba334700fa855
Sania García <santosuke@gmail.com>
2023-11-19 15:03:52 +0100

    add delete posts

--- components/post-container.js
+++ components/post-container.js
@@ -2,6 +2,7 @@ import { use, useEffect, useRef, useState } from "react";
 
 export default function PostContainer({ post }) {
   const [content, setContent] = useState("");
+  const [isDeleted, setIsDeleted] = useState(false);
   const textareaRef = useRef(null);
 
   useEffect(() => {
@@ -30,20 +31,32 @@ export default function PostContainer({ post }) {
     console.log("TEST result", result);
   };
 
+  const handleDelete = async (event) => {
+    event.preventDefault();
+
+    const result = await fetch(`/api/rst/delete?id=${post.id}`);
+    if (result.ok) {
+      setIsDeleted(true);
+    }
+    console.log("TEST result", result);
+  };
+
   return (
-    <div className="w-full flex flex-col gap-3 py-5 border-b-2 border-gray-400/60">
+    <div className={`w-full flex flex-col gap-3 py-5 border-b-2 border-gray-400/60 cursor-default ${isDeleted && "line-through pointer-events-none"}`}>
       <div className="w-full flex justify-between px-5 text-gray-500/50">
-        <a href={`rst/${post.username}`} className="hover:text-gray-500/80">
+        <a href={`rst/${post.username}`} className="hover:text-gray-500/80" disabled>
           {post.username}
         </a>
         <p>{new Date(Number(post.date)).toLocaleString()}</p>
       </div>
       <form onSubmit={handleSubmit}>
-        <textarea ref={textareaRef} rows="1" value={content} onChange={handleChange} wrap="soft" className="w-full p-5 bg-white/20 border-none focus:outline-none resize-none"></textarea>
+        <textarea ref={textareaRef} rows="1" value={content} onChange={handleChange} wrap="soft" className={`w-full p-5 bg-white/20 border-none focus:outline-none resize-none ${isDeleted && "line-through"}`}></textarea>
       </form>
-      <div className="w-full flex justify-between px-5 text-gray-500/50">
-        <button className="hover:text-gray-500/80">Reply</button>
-        <button className="hover:text-gray-500/80">Delete</button>
+      <div className="w-full flex justify-end px-5 text-gray-500/50">
+        {/* <button className="hover:text-gray-500/80">Reply</button> */}
+        <button onClick={handleDelete} className="hover:text-gray-500/80">
+          Delete
+        </button>
       </div>
     </div>
   );
--- pages/api/rst/delete.js
+++ pages/api/rst/delete.js
@@ -1,13 +1,9 @@
 import { sql } from "@vercel/postgres";
-import { v4 as uuidv4 } from "uuid";
-
 export default async function handler(request, response) {
   try {
     const id = request.query.id;
-    const content = request.query.content;
-    if (!id || !content) throw new Error("Id or content missing.");
-    const result = await sql`UPDATE Posts SET Content = ${content} WHERE Id = ${id};`;
-    // console.log("TEST handler result", result);
+    if (!id) throw new Error("Id is missing.");
+    const result = await sql`DELETE FROM Posts WHERE Id = ${id};`;
     return response.status(200).json({ result });
   } catch (error) {
     return response.status(500).json({ error });
